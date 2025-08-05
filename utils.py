0473fc547e8fd15a836023b8797a5525088c8572
roxxorarc <roxxorarc@gmail.com>
2025-08-05 10:03:00 +0200

    add: logger

--- utils.py
+++ utils.py
@@ -1,9 +1,53 @@
 import logging
+import sys
+from typing import Optional
 
 
-def setup_logging():
-    logging.basicConfig(
-        level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
-    )
-    logger = logging.getLogger(__name__)
-    return logger
+class Logger:
+
+    _instance: Optional["Logger"] = None
+    _logger: Optional[logging.Logger] = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __init__(self):
+        if self._logger is None:
+            self._setup_logger()
+
+    def _setup_logger(self):
+        self._logger = logging.getLogger("confluence_to_bookstack")
+        self._logger.setLevel(logging.INFO)
+        if not self._logger.handlers:
+            console_handler = logging.StreamHandler(sys.stdout)
+            console_handler.setLevel(logging.INFO)
+
+            formatter = logging.Formatter(
+                "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+                datefmt="%Y-%m-%d %H:%M:%S",
+            )
+            console_handler.setFormatter(formatter)
+
+            self._logger.addHandler(console_handler)
+
+    def info(self, message: str):
+        self._logger.info(message)
+
+    def error(self, message: str):
+        self._logger.error(message)
+
+    def warning(self, message: str):
+        self._logger.warning(message)
+
+    def debug(self, message: str):
+        self._logger.debug(message)
+
+    def set_level(self, level: int):
+        self._logger.setLevel(level)
+        for handler in self._logger.handlers:
+            handler.setLevel(level)
+
+
+logger = Logger()
